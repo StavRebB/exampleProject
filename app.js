304e1f3102962e72bed3ee66d806fc7c6e650390
StavRebB <stavrebb@yahoo.com>
2021-01-28 08:53:59 +0200

    call function

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ var var2 = 7;
 var getSum = function (num1, num2) {
     return num1 + num2;
 };
+console.log(getSum(var1, var2));
--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 let var1:number = 5
-let var2:number = 7;
+let var2:number = 7
 let getSum = (num1:number, num2:number):number => {
-    return num1+num2;
+    return num1+num2
 }
+console.log(getSum(var1,var2))
